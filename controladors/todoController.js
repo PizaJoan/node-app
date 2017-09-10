907d9fc550709488e93d00cf5445e3c362cf0f45
Joan Piza Ferra <pizajoan@MacBook-Pro-de-Joan.local>
2017-09-10 12:39:32 +0200

    Creada conexio bbdd amb mongo hosteat per mlab

--- controladors/todoController.js
+++ controladors/todoController.js
@@ -1,5 +1,19 @@
-let bodyParser = require('body-parser')
-let urlencodedParser = bodyParser.urlencoded({extended: false})
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({extended: false})
+
+//Base de dades mongo DB hosteat per mlab
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://test:123@ds131914.mlab.com:31914/todo')
+//Crear un esquema i model perque mongo sapiga com han de entrar les dades
+const esquema = new mongoose.Schema({
+	item: String
+})
+const Model = mongoose.model('Model', esquema)
+
+let itemOne = Model({item: 'Primer test'}).save(err => {
+	if (err) throw err
+	console.log('guardat...')
+})
 
 let data = [{
 	item: 'get milk'
